f17cee76f13004e340ed7335856bc8aaea4c0755
LLergibt <gosha2007naz.com>
2026-05-02 14:56:04 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ def create_app() -> FastAPI:
 
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173", "http://localhost:3000"],
+        allow_origins=["http://localhost:5173", "http://localhost:3000", "https://search-queries.vercel.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
